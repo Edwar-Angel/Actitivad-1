387e22a3306f3eda4704831e62e617bb3b8775cb
Edwar-Angel <36248220+Edwar-Angel@users.noreply.github.com>
2018-02-25 13:03:09 -0500

    Update and rename Base de datos/consultas.sql to  Base de datos/consultas.sql

--- Base de datos/consultas.sql
+++ Base de datos/consultas.sql
@@ -11,3 +11,6 @@ SELECT autor,ano FROM proyecto WHERE ano <='2012'
 SELECT carrera.nombre as carrera ,usuarios.nombre,usuario,correo FROM carrera INNER JOIN usuarios on usuarios.id_carrera=carrera.id WHERE usuarios.id_carrera= '1'
 
 #debuelve la carrera, nombre, usuario y correo de un usuario registrado en Informatíca
+
+SELECT proyecto.nombre as proyecto, carrera.nombre as carrera FROM proyecto INNER JOIN carrera on carrera.id=id_carrera
+# debuelbe todos los proyectos por carrera 
